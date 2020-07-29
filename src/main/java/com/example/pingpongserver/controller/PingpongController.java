41905520dd3910e39eb4f6af66b7ef0f33243c73
ihcomega56 <ihcomega56@gmail.com>
2020-07-30 05:25:25 +0900

    renamed a file

--- src/main/java/com/example/pingpongserver/controller/PingpongController.java
+++ src/main/java/com/example/pingpongserver/controller/PingpongController.java
@@ -3,7 +3,7 @@ package com.example.pingpongserver.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class PingPongController {
+public class PingpongController {
 
     @RequestMapping("/pingpong")
     public String index(@RequestBody String body) {
