20cd7a3a15ab193ce7db24c211624ef442810f9a
ihcomega56 <ihcomega56@gmail.com>
2020-11-04 13:01:40 +0900

    Add API for demos

--- src/main/java/com/example/pingpongserver/controller/PingpongController.java
+++ src/main/java/com/example/pingpongserver/controller/PingpongController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class PingpongController {
 
+    @RequestMapping("/version")
+    public String index() {
+        return "現在のバージョンは0.1.0です"; // TODO: 設定ファイルから取る
+    }
+
     @RequestMapping("/pingpong")
     public String index(@RequestBody String body) {
         return body;
